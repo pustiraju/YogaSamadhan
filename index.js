80d5656728622c2ca5580ce18cffd7845a3dd23f
Raju Pusti <rajupusti@yahoo.com>
2025-04-04 14:00:43 +0000

    update

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 
-// Serve static files from "public" folder
-app.use(express.static('public'));
+const app = express();
 
-// Serve index.html from views folder
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get('/courses', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get('/gallery', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
 
 app.listen(3000, () => {
-  console.log('Server is running at http://localhost:3000');
+  console.log('Server is running on http://127.0.0.1:3000');
 });
