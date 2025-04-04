ced8b966eca9c8a3fcd01f35decdab62307e07e6
Raju Pusti <rajupusti@yahoo.com>
2025-04-04 04:51:53 +0000

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html from views folder
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Server is running at http://localhost:3000');
+});
